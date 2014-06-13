5b893a468470b8e4cc4df0e585233f40eb3aa9a6
Chao Qu <quchao@seas.upenn.edu>
2014-06-13 00:57:03 -0400

    refactor ros node

--- apriltag_node/src/apriltag_node.cpp
+++ apriltag_node/src/apriltag_node.cpp
@@ -1,12 +1,7 @@
-/*
- *  main.cpp
- *  Demo code for detecting AprilTags in ROS
- *  Added by gareth on April 28th, 2014
- */
-
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -14,7 +9,7 @@
 
 #include <vector>
 
-#define BUILD_UMICH
+#define BUILD_MIT
 
 #if defined(BUILD_UMICH)
 extern "C" {
@@ -28,39 +23,35 @@ extern "C" {
 #include "AprilTags/Tag36h11.h"
 #endif
 
-void cam_callback(const sensor_msgs::Image::ConstPtr &img) {
-  //    convert to greyscale
-  cv::Mat image_gray;
+const cv::Scalar colors[] = {cv::Scalar(255, 0, 0, 0), cv::Scalar(0, 255, 0, 0),
+                             cv::Scalar(0, 0, 255, 0),
+                             cv::Scalar(255, 0, 255, 0)};
 
-  if (img->encoding == sensor_msgs::image_encodings::MONO8) {
-    //  use existing data
-    image_gray = cv::Mat(cv::Size(img->width, img->height), CV_8UC1,
-                         const_cast<uchar *>(&img->data[0]), img->step);
+class AprilTag {
+ public:
 
-    // ROS_INFO("Made MONO8 image");
-  } else {
-    ROS_WARN("Unsupported image encoding: %s", img->encoding.c_str());
-    return;
-  }
+ private:
+
+};  // class AprilTag
+
+void cam_callback(const sensor_msgs::ImageConstPtr &image,
+                  const sensor_msgs::CameraInfoConstPtr $cinfo) {
+  // use cv_bridge and convert to grayscale image
+  cv_bridge::CvImagePtr cv_ptr;
+  cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::MONO8);
 
   std::vector<cv::Point2f> corners;
 
-  cv::Mat display;
-  cv::cvtColor(image_gray, display, CV_GRAY2RGB);
+  cv::Mat image_rgb;
+  cv::cvtColor(cv_ptr->image, image_rgb, CV_GRAY2RGB);
 
 #if defined(BUILD_UMICH)
-
-  // ROS_INFO("Looking for tags");
-
+  // use apriltag_umich
   static april_tag_family_t *tf = tag36h11_create();
   static april_tag_detector_t *td = april_tag_detector_create(tf);
 
-  image_u8_t *im = image_u8_create(image_gray.cols, image_gray.rows);
-  //  lazy, just copy for now
-  memcpy(im->buf, image_gray.ptr(), image_gray.cols * image_gray.rows);
-  im->stride = img->width;
-
-  //  detect tags
+  image_u8_t *im = image_u8_create_from_gray(
+      cv_ptr->image.cols, cv_ptr->image.rows, cv_ptr->image.data);
   zarray_t *detections = april_tag_detector_detect(td, im);
 
   for (size_t i = 0; i < zarray_size(detections); i++) {
@@ -68,9 +59,9 @@ void cam_callback(const sensor_msgs::Image::ConstPtr &img) {
     zarray_get(detections, i, &det);
 
     for (int j = 0; j < 4; j++) {
-      corners.push_back(cv::Point2f(det->p[j][0], det->p[j][1]));
+      const cv::Point2f p = cv::Point2f(det->p[j][0], det->p[j][1]);
+      corners.push_back(p);
     }
-
     april_tag_detection_destroy(det);
   }
 
@@ -78,45 +69,39 @@ void cam_callback(const sensor_msgs::Image::ConstPtr &img) {
   image_u8_destroy(im);
 
 #elif defined(BUILD_MIT)
-
+  // Use apriltag_mit
   static AprilTags::TagDetector tag_detector(AprilTags::tagCodes36h11);
-  vector<AprilTags::TagDetection> detections =
-      tag_detector.extractTags(image_gray);
+  std::vector<AprilTags::TagDetection> detections =
+      tag_detector.extractTags(cv_ptr->image);
 
-  for (vector<AprilTags::TagDetection>::iterator I = detections.begin();
-       I != detections.end(); I++) {
+  for (std::vector<AprilTags::TagDetection>::iterator iter = detections.begin();
+       iter != detections.end(); iter++) {
     for (int j = 0; j < 4; j++) {
-      const cv::Point2f p = cv::Point2f(I->p[j].first, I->p[j].second);
+      const cv::Point2f p = cv::Point2f(iter->p[j].first, iter->p[j].second);
       corners.push_back(p);
     }
   }
 
 #endif
 
-  ROS_INFO("Found %lu corners\n", corners.size());
-
-  //  draw and display
-  for (size_t i = 0; i < corners.size(); i++) {
-    cv::circle(display, corners[i], 8, cv::Scalar(255, 0, 0, 0), 2);
-  }
-
-  cv::imshow("camera", display);
+  cv::imshow("image", image_rgb);
   cv::waitKey(1);
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "test_apriltags");
-  ros::NodeHandle n;
-  image_transport::ImageTransport imgTransport(n);
+  ros::init(argc, argv, "apriltag_node");
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it(nh);
 
-  //    subscribe to camera
-  image_transport::Subscriber camSub =
-      imgTransport.subscribe("/camera/image_raw", 1, cam_callback);
+  // subscribe to camera
+  image_transport::CameraSubscriber camera_sub =
+      it.subscribeCamera("image_raw", 1, cam_callback);
 
-  //    output for OpenCV
-  cv::namedWindow("camera", 1);
+  // output for OpenCV
+  cv::namedWindow("image", 1);
 
   ros::spin();
+
   return 0;
 }
