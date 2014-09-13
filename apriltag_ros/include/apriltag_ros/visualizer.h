7f686a57375dab20dbae4a9929ca2baafc5c3551
Chao Qu <quchao@seas.upenn.edu>
2014-09-13 12:37:46 -0400

    use rviz_helpers color

--- apriltag_ros/include/apriltag_ros/visualizer.h
+++ apriltag_ros/include/apriltag_ros/visualizer.h
@@ -5,19 +5,11 @@
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <rviz/helpers/color.h>
+#include <rviz_helper/colors.h>
 #include <apriltag_ros/Apriltag.h>
 #include <apriltag_ros/Apriltags.h>
 
 namespace apriltag_ros {
-
-namespace colors {
-const rviz::Color RED = {1, 0, 0};
-const rviz::Color GREEN = {0, 1, 0};
-const rviz::Color BLUE = {0, 0, 1};
-const rviz::Color CYAN = {0, 1, 1};
-const rviz::Color MAGENTA = {1, 0, 1};
-const rviz::Color YELLOW = {1, 1, 0};
-}
 
 class ApriltagVisualizer {
  public:
